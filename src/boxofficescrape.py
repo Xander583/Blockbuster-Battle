b1afb9138506e4ef3d7cd737269e4a7849625fdd
abergeron21 <98467752+abergeron21@users.noreply.github.com>
2023-12-18 18:19:08 -0500

    Update boxofficescrape.py

--- src/boxofficescrape.py
+++ src/boxofficescrape.py
@@ -4,16 +4,19 @@ import datetime
 import requests
 import pandas as pd
 from requests_html import HTML
+import gspread
+from google.oauth2 import service_account
+import gspread_dataframe as gd
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def url_to_txt(url, filename="weekly.html", save=False):
+def url_to_txt(url, filename="boxoffice.csv", save=True):
     r = requests.get(url)
     if r.status_code == 200:
         html_text = r.text
         if save:
-            with open(filename, 'w') as f:
+            with open(filename, 'w', encoding='utf-8') as f:
                 f.write(html_text)
         return html_text
     return None
@@ -47,17 +50,19 @@ def parse_and_extract(url, week_number):
 
     df = pd.DataFrame(table_data, columns=header_names)
 
-    path = os.path.join(BASE_DIR, 'data')
-    os.makedirs(path, exist_ok=True)
-    filepath = os.path.join('data', f'week-{week_number}.csv')
-    df.to_csv(filepath, index=False)
+    # Select only the "Release" and "Gross" columns
+    df_selected = df[["Release", "Gross"]]
+
+    filepath = os.path.join(BASE_DIR, 'boxoffice.csv')
+    df_selected.to_csv(filepath, index=False)
+    
     return True
 
 
 def run(start_week=None, weeks_ago=0):
     if start_week is None:
         now = datetime.datetime.now()
-        for i in range(weeks_ago + 1):
+        for i in range(3):
             start_week = now - datetime.timedelta(weeks=i)
             year = start_week.strftime("%Y")
             week_number = start_week.strftime("%U")
@@ -76,3 +81,30 @@ if __name__ == "__main__":
     except:
         count = 0
     run(weeks_ago=count)
+
+# Code for updating google sheet with new .csv
+csv_file_path = 'boxoffice.csv' 
+google_sheet_name = 'BlockbusterTest'
+google_sheet_key = 'BBKey.json' 
+
+# Set up Google Sheets API credentials
+creds = service_account.Credentials.from_service_account_file(google_sheet_key, scopes=['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive'])
+
+# Authorize with Google Sheets
+client = gspread.Client(auth=creds)
+client.login()
+
+# Open the Google Sheet
+spreadsheet = client.open(google_sheet_name)
+worksheet = spreadsheet.get_worksheet(0)  # Assumes you want to work with the first worksheet
+
+# Clear the contents of the Google Sheet
+worksheet.clear()
+
+# Read the CSV file into a DataFrame
+df = pd.read_csv(csv_file_path)
+
+# Update the Google Sheet with the selected columns
+gd.set_with_dataframe(worksheet, df[["Release", "Gross"]])
+
+print(f"Selected data has been uploaded to '{google_sheet_name}' on Google Sheets.")
